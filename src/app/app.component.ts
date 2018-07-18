f0e57877d0602e1ec22e95917c579356b9391acd
Matteo Lombardi <lombardi.matteo.1005@gmail.com>
2018-07-18 15:13:13 +0200

    Removed simulated DB

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
-import {GraficoService} from './grafico.service';
-import {Chart} from 'chart.js'; 
+import { GraficoService } from './grafico.service';
+import { Chart } from 'chart.js';
 
 
 @Component({
@@ -9,59 +9,53 @@ import {Chart} from 'chart.js';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public names:string[];
-  public scores:number[];
-  public coloriGrafico:string[];
-  public chart:Chart;
-  constructor(private _grafico:GraficoService){
-    this.names=[];
-    this.scores=[];
-    this.coloriGrafico=[];
+  public names: string[];
+  public scores: number[];
+  public coloriGrafico: string[];
+  public chart: Chart;
+  constructor(private _grafico: GraficoService) {
+    this.names = [];
+    this.scores = [];
+    this.coloriGrafico = this.colorGenerator();
   }
 
 
-  ngOnInit(){
+  ngOnInit() {
     this._grafico.graficoClienti()
-    .subscribe(res => {
-        for (let i:number=0; i<Object.keys(res).length; i++){
-          this.names.push(res[i].nome);
-          this.scores.push(res[i].score); 
-        }
-        console.log(this.scores);
-        console.log(this.names);
-        this.colorGenerator();
-    });
-    var canvas = <HTMLCanvasElement> document.getElementById("pippo");
-    if(canvas!=null){
-      var ctx: CanvasRenderingContext2D = canvas.getContext("2d");
-      //var ctx = <CanvasRenderingContext2D> document.getElementById("pippo");
-      //.getContext('2d');
-      this.chart = new Chart(ctx, {
-        type:'bar',
-        data:{
-          labels: this.names,
-          datasets:[{
-            data: this.scores,
-            borderColor: '#1b1b1b',
-            backgroundColor: this.coloriGrafico,
-            fill: true
-          }]
-        },
-        options:{
-          legend:{
-            display:false
+      .subscribe((res: any[]) => {
+
+        res.forEach(({ nome, score }) => {
+          this.names.push(nome);
+          this.scores.push(score);
+        });
+
+        var canvas = <HTMLCanvasElement>document.getElementById("pippo");
+        var ctx: CanvasRenderingContext2D = canvas.getContext("2d");
+        //var ctx = <CanvasRenderingContext2D> document.getElementById("pippo");
+        //.getContext('2d');
+        this.chart = new Chart(ctx, {
+          type: 'bar',
+          data: {
+            labels: this.names,
+            datasets: [{
+              data: this.scores,
+              borderColor: '#1b1b1b',
+              backgroundColor: this.coloriGrafico,
+              fill: true
+            }]
+          },
+          options: {
+            legend: {
+              display: false
+            }
           }
-        }
+        });
       });
-      }
-    
+
   }
-  public colorGenerator(){
-    let color:string;
-    for (let i:number=0; i<900; i++){
-    color = "#" + Math.random().toString(16).slice(2, 8);
-    console.log(color);
-    this.coloriGrafico.push(color); 
-    }
-}
+  public colorGenerator(num = 900) {
+    const res = new Array(num);
+    res.fill('');
+    return res.map(() => "#" + Math.random().toString(16).slice(2, 8));
+  }
 }
