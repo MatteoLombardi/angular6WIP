f7f99a6bd8abe203954c08214954169d3a7e9f7b
Matteo Lombardi <lombardi.matteo.1005@gmail.com>
2018-07-18 12:03:48 +0200

    chart design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,21 +11,25 @@ import {Chart} from 'chart.js';
 export class AppComponent {
   public names:string[];
   public scores:number[];
+  public coloriGrafico:string[];
   public chart:Chart;
   constructor(private _grafico:GraficoService){
     this.names=[];
     this.scores=[];
+    this.coloriGrafico=[];
   }
+
 
   ngOnInit(){
     this._grafico.graficoClienti()
     .subscribe(res => {
         for (let i:number=0; i<Object.keys(res).length; i++){
           this.names.push(res[i].nome);
-          this.scores.push(res[i].score);
-          console.log(this.scores);
+          this.scores.push(res[i].score); 
         }
-        
+        console.log(this.scores);
+        console.log(this.names);
+        this.colorGenerator();
     });
     var canvas = <HTMLCanvasElement> document.getElementById("pippo");
     if(canvas!=null){
@@ -39,6 +43,7 @@ export class AppComponent {
           datasets:[{
             data: this.scores,
             borderColor: '#1b1b1b',
+            backgroundColor: this.coloriGrafico,
             fill: true
           }]
         },
@@ -51,4 +56,12 @@ export class AppComponent {
       }
     
   }
+  public colorGenerator(){
+    let color:string;
+    for (let i:number=0; i<900; i++){
+    color = "#" + Math.random().toString(16).slice(2, 8);
+    console.log(color);
+    this.coloriGrafico.push(color); 
+    }
+}
 }
