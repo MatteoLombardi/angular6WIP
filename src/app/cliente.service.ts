68ff88ba782ff2e42acdb416c114879545433ec8
Matteo Lombardi <lombardi.matteo.1005@gmail.com>
2018-07-23 10:31:32 +0200

    UI Fix

--- src/app/cliente.service.ts
+++ src/app/cliente.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
      
 import { Observable, of } from 'rxjs';
- 
+
 import { Cliente } from './cliente';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { MessaggioService } from './messaggio.service';
